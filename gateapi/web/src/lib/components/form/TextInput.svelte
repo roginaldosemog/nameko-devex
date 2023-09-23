8f6521a9401660cc9866236abf4278f679e7b7a0
Igor Gomes <roginaldosemog@gmail.com>
2023-09-23 17:23:12 -0300

    feat: Create TextInput component

---
+++ gateapi/web/src/lib/components/form/TextInput.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let label,
+		name,
+		type,
+		placeholder = ''
+</script>
+
+<label class="flex flex-col">
+	{label}
+	<input {name} {type} {placeholder} />
+</label>
+
+<style lang="scss">
+	label {
+		font-size: 16px;
+		font-family: amplitude-wide;
+		font-weight: 500;
+	}
+
+	input {
+		border-radius: 4px;
+		height: 48px;
+		border: 1px solid #c3c1d7;
+
+		font-size: 16px;
+		font-weight: 300;
+		font-family: amplitude-wide;
+
+		transition: box-shadow 200ms;
+
+		&:focus {
+			border: 1px solid #c3c1d7;
+			box-shadow: 0 4px 4px rgba($color: #000000, $alpha: 0.25);
+		}
+	}
+</style>
--- gateapi/web/src/routes/property-details/+page.svelte
+++ gateapi/web/src/routes/property-details/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import PropertyImage from '$lib/components/PropertyImage.svelte'
+	import TextInput from '$lib/components/form/TextInput.svelte'
 </script>
 
 <svelte:head>
@@ -7,17 +8,23 @@
 </svelte:head>
 
 <div class="container min-h-screen">
-	<section class="pt-4">
+	<section class="pt-11">
 		<div class="flex gap-x-5">
 			<div class="basis-full sm:basis-1/2">
 				<h1 class="text-3xl md:text-4xl pb-2">Property details</h1>
-				<p class="text-lg">
+				<p class="text-lg pb-11">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
 					minim veniam.
 				</p>
+				<div class="w-full pb-4">
+					<TextInput label="Property Name" name="property-name" type="text" />
+				</div>
+				<div class="w-full">
+					<TextInput label="Address" name="address" type="text" />
+				</div>
 			</div>
-			<div class="hidden sm:flex basis-1/2 justify-end items-start">
+			<div class="hidden sm:flex basis-1/2 justify-end items-center">
 				<PropertyImage />
 			</div>
 		</div>
